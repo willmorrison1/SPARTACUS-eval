4e743cea6ab7a2d2530f93caeccc88b364522e2b
willmorrison1 <willmorrison661@gmail.com>
2020-03-20 01:18:45 +0000

    iterate other vars

--- R/RB3Dstats_SW_perType.R
+++ R/RB3Dstats_SW_perType.R
@@ -15,7 +15,6 @@ if (isLocalWindows) {
   baseProDir <- "/storage/basic/micromet/"
 }
 
-
 plotDir <- file.path(baseProDir, "/Tier_processing/hv867657/SPARTACUS/")
 dartSimulationDir <- file.path(baseProDir, "/Tier_processing/hv867657/DART_5-7-5_1126/user_data/simulations")
 #the DEM that is exactly centered on the DART simulation domain.
@@ -75,33 +74,39 @@ seqParams <- daRt::sequenceParameters(allFiles)
 
 iterNo <- 1 #preallocate a tracker for the iteration(s)
 for (i in 1:length(simDir)) {
-  print(paste(iterNo, "/", length(simDir)))
-  sF <- daRt::simulationFilter(
-    variables = "RADIATIVE_BUDGET",
-    product = "rb3D", iters = iterVals,
-    variablesRB3D = variablesRB3DVals,
-    bands = 0L,
-    typeNums = typeNumsAll)
-  d <- daRt::getData(x = simDir[i], sF = sF, nCores = 1)
-  d1 <- daRt::rb3DtoNc(x = d)
-  rm(d); gc()
-
-  d1 <- daRt::removeRelief(x = d1, DEM = DEM,
-                           maxUndergroundCells = maxUndergroundCellsVal,
-                           fun = DEMaggregateFun)
-
-  zStatsList[[iterNo]] <- as.data.frame(d1) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(typeNum_raw = typeNum) %>%
-    dplyr::mutate(typeNum = typeNumConvert(typeNum)) %>%
-    dplyr::group_by(Z, variableRB3D, band, iter, typeNum, simName) %>%
-    dplyr::summarise(nTypeNos = length(unique(typeNum_raw)),
-                     #merge values for sub-groups into values for final groups, as per typeNumConvert().
-                     #e.g. sub groups of north, , etc.. walls are merged to give one value for "Walls" group.
-                     #as the sub-group sample sizes are the same, the below equates to the mean of the sum
-                     #of any grouped values
-                     meanVal = mean(value) * nTypeNos)
-  iterNo <- iterNo + 1
+  for (v in 1:length(iterVals)) {
+    for (z in 1:length(variablesRB3DVals)) {
+      
+      print(paste(iterNo, "/", length(simDir)))
+      sF <- daRt::simulationFilter(
+        variables = "RADIATIVE_BUDGET",
+        product = "rb3D", iters = iterVals[v],
+        variablesRB3D = variablesRB3DVals[z],
+        bands = 0L,
+        typeNums = typeNumsAll)
+      d <- daRt::getData(x = simDir[i], sF = sF, nCores = 1)
+      d1 <- daRt::rb3DtoNc(x = d)
+      rm(d); gc()
+      
+      d1 <- daRt::removeRelief(x = d1, DEM = DEM,
+                               maxUndergroundCells = maxUndergroundCellsVal,
+                               fun = DEMaggregateFun)
+      
+      zStatsList[[iterNo]] <- as.data.frame(d1) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(typeNum_raw = typeNum) %>%
+        dplyr::mutate(typeNum = typeNumConvert(typeNum)) %>%
+        dplyr::group_by(Z, variableRB3D, band, iter, typeNum, simName) %>%
+        dplyr::summarise(nTypeNos = length(unique(typeNum_raw)),
+                         #merge values for sub-groups into values for final groups, as per typeNumConvert().
+                         #e.g. sub groups of north, , etc.. walls are merged to give one value for "Walls" group.
+                         #as the sub-group sample sizes are the same, the below equates to the mean of the sum
+                         #of any grouped values
+                         meanVal = mean(value) * nTypeNos)
+      iterNo <- iterNo + 1
+      
+    }
+  }
 }
 
 zStats <- dplyr::bind_rows(zStatsList)
